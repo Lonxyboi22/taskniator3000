f1956eb7cd385a68eb040466d9c59f3c14c5d275
Lonxyboi <drewthemachine22@gmail.com>
2022-04-25 19:17:54 -0400

    added more stuff to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,9 @@ console.log(buttonE1);
 buttonE1.addEventListener("click", function(){
     alert("button clicked");
 })
+
+
+
+// var taskItem1 = document.createElement("li");
+// taskItem1.textContent = "hello"
+// console.log(taskItem1);
